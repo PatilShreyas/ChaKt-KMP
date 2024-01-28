d336b3d7535747451fbe67d27ef198566d478a1e
Shreyas Patil <shreyaspatilg@gmail.com>
2024-01-28 21:52:37 +0530

    Fix the path of linux executable jar

--- buildLinux.sh
+++ buildLinux.sh
@@ -4,4 +4,4 @@
 mkdir distributions
 echo "Building Linux Desktop App 🖥"
 ./gradlew :composeApp:packageUberJarForCurrentOS
-cp composeApp/build/compose/jars/chakt_1.0.0-1_amd64.jar distributions/chakt-linux-amd64.jar
+cp composeApp/build/compose/jars/ChaKt-linux-x64-1.0.0.jar distributions/chakt-linux-x64.jar
